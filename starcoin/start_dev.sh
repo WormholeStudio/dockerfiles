affea170a7e1667871983c59900b9f86a815b078
antares <antares@wormhole.studio>
2021-08-21 10:09:33 +0800

    add

--- starcoin/start_dev.sh
+++ starcoin/start_dev.sh
@@ -1,5 +1,4 @@
-docker run  --rm -it \
-    -v data:/data/starcoin/ \
-     -p 9850:9850  \
+docker run  --rm -it  \
+    -v /Users/reilost/Dropbox/wormhole/mobius/dockerfiles/starcoin/data:/data/starcoin \
     starcoin/starcoin:sha-6ba7f68 \
-    /starcoin/starcoin   -n mobius_dev:66   -d /data/starcoin  console
+    /starcoin/starcoin   -n mobius_dev:66   -d /data/starcoin console
