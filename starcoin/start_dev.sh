e9e8ae5c2c3a3d00c92e184824d170f078197f0e
antares <antares@wormhole.studio>
2021-08-21 10:14:21 +0800

    Add

--- starcoin/start_dev.sh
+++ starcoin/start_dev.sh
@@ -1,4 +1,5 @@
 docker run  --rm -it  \
     -v /Users/reilost/Dropbox/wormhole/mobius/dockerfiles/starcoin/data:/data/starcoin \
     starcoin/starcoin:sha-6ba7f68 \
-    /starcoin/starcoin   -n mobius_dev:66   -d /data/starcoin console
+    /starcoin/starcoin   -n mobius_dev:66     --disable-metrics true  --miner-thread 0  \
+    -d /data/starcoin console
